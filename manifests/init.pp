ec506ea13fc7bf6f0716d539000387c4ce8ede73
Alessandro Franceschi <al@example42.com>
2020-12-07 19:24:09 +0100

    Added options and hiera.yaml layers

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,34 @@
 # Directory apply contains necessary manifests and scripts.
 # Directory tasks contains a task to deploy the application.
 #
+# @param service Optional service(s) to manage (value can be a String,
+#   and Array or an Hash of service resources.
+# @param service_params Parameters to add to the service resource(s).
+#   If $service is an hash, this is merged with the hash values for each
+#   service. Default is to start a service at boot time and ensure is running.
+# @param service_notify When true service is restarted whenever a file
+#   is changed.
+# @param package Optional package(s) to manage (value can be a String,
+#   and Array or an Hash of package resources.
+# @param package_params Parameters to add to the package resource(s).
+#   If $package is an hash, this is merged with the hash values for each
+#   package. Default is to ensure the package is present.
+# @param user Optional user related to my_app. If set, all files set in $files
+#   by default will be owned by this user.
+# @param user_create. If to create the above $user. If $user is set, $files are
+#   managed and $user_create is false, then $user must exist on the system.
+# @param user_params Parameters to add to the user resource.
+# @param group Optional group related to my_app. If set, all files set in $files
+#   by default will be owned by this group.
+# @param group_create. If to create the above $group. If $group is set, $files are
+#   managed and $group_create is false, then $group must exist on the system.
+# @param group_params Parameters to add to the group resource.
+# @param files An hash of Puppet file resources to manage. This files will
+#   automatically notify $service, if $service_notify is true and will be owned
+#   by $user and $group, if they are set.
+# @param options An hash of custom options to use in templates specified in the
+#   $files or $resources params. Use this to customize different settings that
+#   you may want to set, via Hiera, for the same files on different nodes.
 # @param resources An hash of an Hash of any Puppet resource type to apply.
 #   Consider it as a catch all way to set on Hiera any resource of any type.
 #   You can always specify for each resource type the default parameters via
@@ -19,6 +47,24 @@
 #   merge behaviour configured via $resources_defaults_merge_behaviour
 # @param resources_defaults_merge_behaviour Defines the lookup method to use to
 #   retrieve via hiera the my_app::resources_defaults key
+#
+# @example Set custom files with relevant options, and manage my_app service
+#   as String and package as Hash, with package version to install.
+#     my_app::service: my_app
+#     my_app::package:
+#       my_app:
+#         ensure: v2.0.1
+#     my_app::files:
+#       /etc/my_app/my_app.conf:
+#         content: template('my_app/my_app.conf')
+#       /etc/my_app/users:
+#         content: template('my_app/users')
+#         mode: 0400
+#     my_app::options:
+#       db_host: db.example.com
+#       db_database: my_app-prod
+#       db_user: my_app-prod
+#       db_password: s3cr3t # Consider using eyaml or other to encrypt secrets
 #
 # @example Define arbitrary resources, with some defaults
 #     my_app::resources:
@@ -52,17 +98,26 @@ class my_app (
   Boolean $user_create         = false,
   Hash $user_params            = { },
 
+  Variant[Undef,String] $group = undef,
+  Boolean $group_create        = false,
+  Hash $group_params           = { },
+
+  Hash $files                  = { },
+  Hash $options                = { },
+
   # Hash $resources (lookup with $resources_merge_behaviour)                   = {},
   # Hash $resources_defaults (lookup with $resources_defaults_merge_behaviour) = {},
   Enum['first','hash','deep'] $resources_merge_behaviour          = 'deep',
   Enum['first','hash','deep'] $resources_defaults_merge_behaviour = 'deep',
 ) {
 
+  # Manage service
   case $service {
     String: {
       service { $service:
         * => $service_params,
       }
+      $file_notify = $service
     }
     Array: {
       $service.each | $s | {
@@ -70,6 +125,7 @@ class my_app (
           * => $service_params,
         }
       }
+      $file_notify = $service
     }
     Hash: {
       $service.each | $s,$p | {
@@ -77,10 +133,12 @@ class my_app (
           * => $service_params + $p,
         }
       }
+      $file_notify = keys($service)
     }
     default: {}
   }
 
+  # Manage package
   case $package {
     String: {
       package { $package:
@@ -104,17 +162,26 @@ class my_app (
     default: {}
   }
 
-  if $service_notify {
+  # Manage files
+  if $service_notify and $service {
     File {
-      notify => Service[$service],
+      notify => $file_notify,
+      owner  => $user,
+      group  => $group,
     }
   }
 
-  # General resources configured via Hiera
+  $files.each | $k,$v | {
+    file { $k:
+      * => $v,
+    }
+  }
+
+  # Manage generic resources
   $resources = lookup('my_app::resources',Hash,$resources_merge_behaviour,{})
   $resources_defaults = lookup('my_app::resources_defaults',Hash,$resources_defaults_merge_behaviour,{})
 
-  $resources.each |$k,$v| {
+  $resources.each | $k,$v | {
     if $k in keys($resources_defaults) {
       $resource_defaults = $resources_defaults[$k]
     } else {
